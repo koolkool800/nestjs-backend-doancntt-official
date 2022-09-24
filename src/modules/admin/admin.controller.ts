617ae4fedcac1fa5ddd52ffc2cf9fb98f14062bd
barokool <knabao7a7@gmail.com>
2022-09-24 10:28:57 +0700

    feature : login & register admin route, just validate - no more

--- src/modules/admin/admin.controller.ts
+++ src/modules/admin/admin.controller.ts
@@ -1,7 +1,44 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { SignInInput, SignUpInput } from 'src/auth/dto/auth.dto';
 import { AdminService } from './admin.service';
 
 @Controller('admin')
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
+
+  @Post('')
+  async createSuperAdmin(
+    @Res() response,
+    @Body() input: SignUpInput,
+  ): Promise<any> {
+    const createdAdmin = await this.adminService.signUpSuperAdmin(input);
+    if (createdAdmin) {
+      return response.status(HttpStatus.OK).json({
+        msg: 'Created admin success',
+        data: createdAdmin,
+      });
+    }
+    return response.status(HttpStatus.BAD_REQUEST).json({
+      msg: 'Created admin failure',
+      data: null,
+    });
+  }
+
+  @Post('/login')
+  async loginAdmin(@Res() response, @Body() input: SignInInput): Promise<any> {
+    const user = await this.adminService.loginAdmin(input);
+
+    if (user) {
+      const cloneUser = { ...user };
+      delete cloneUser['password'];
+      return response.status(HttpStatus.OK).json({
+        msg: 'Login success',
+        data: cloneUser,
+      });
+    }
+    return response.status(HttpStatus.BAD_REQUEST).json({
+      msg: 'Login failure',
+      data: null,
+    });
+  }
 }
