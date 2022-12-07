d73bf1b8b4f5f94330aea609e61994c95c054c8a
barokool <knabao7a7@gmail.com>
2022-12-07 09:25:41 +0700

    cmmt uploads

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -14,27 +14,27 @@ import { extname, join } from 'path';
 export class AppController {
   constructor() {}
 
-  @Post('')
-  @UseInterceptors(
-    FileInterceptor('file', {
-      storage: diskStorage({
-        destination: './uploads',
-        filename: (req, file, callback) => {
-          const uniqueSuffix =
-            Date.now() + '-' + Math.round(Math.random() * 1e9);
-          const ext = extname(file.originalname);
-          const filename = `${uniqueSuffix}${ext}`;
-          callback(null, filename);
-        },
-      }),
-    }),
-  )
-  uploadImage(@UploadedFile() file: Express.Multer.File) {
-    console.log(file);
-  }
+  // @Post('')
+  // @UseInterceptors(
+  //   FileInterceptor('file', {
+  //     storage: diskStorage({
+  //       destination: './uploads',
+  //       filename: (req, file, callback) => {
+  //         const uniqueSuffix =
+  //           Date.now() + '-' + Math.round(Math.random() * 1e9);
+  //         const ext = extname(file.originalname);
+  //         const filename = `${uniqueSuffix}${ext}`;
+  //         callback(null, filename);
+  //       },
+  //     }),
+  //   }),
+  // )
+  // uploadImage(@UploadedFile() file: Express.Multer.File) {
+  //   console.log(file);
+  // }
 
-  @Get('/:imageName')
-  getImage(@Param('imageName') imageName, @Res() res) {
-    res.sendFile(join(process.cwd(), 'uploads/' + imageName));
-  }
+  // @Get('/:imageName')
+  // getImage(@Param('imageName') imageName, @Res() res) {
+  //   res.sendFile(join(process.cwd(), 'uploads/' + imageName));
+  // }
 }
